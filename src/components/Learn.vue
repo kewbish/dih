667005d6199d18f8da508bd2e26b7393e671de60
Kewbish <kewbish@gmail.com>
2020-04-10 19:46:24 -0700

    Finish slide

--- src/components/Learn.vue
+++ src/components/Learn.vue
@@ -11,7 +11,7 @@
         </div>
       </div>
       <div v-if="auth">
-        <slide :slideSubject="'Hello ' + firstName + '!'" :slideContent="[{line: 'Time to get started on your web development journey!'},{line:'First, let\'s download some software.'},{img: `https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png`}]"></slide>
+        <slide v-if="count == 1" :slideSubject="'Hello ' + firstName + '!'" :slideContent="[{line: 'Time to get started on your web development journey!'},{line:'First, let\'s download some software.'},{img: `https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png`},{list:[{item: 'hello'}, {item: 'hi'}]}]"></slide>
       </div>
     </div>
   </section>
@@ -26,7 +26,8 @@ export default {
       user: {
         logged: false,
         data: {}
-      }
+      },
+      count: 1
     };
   },
   computed: {
--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,9 +1,12 @@
 <template>
     <section class="section">
         <h1 class="title is-1">{{ slideSubject }}</h1>
-        <p v-for="s in slideContent" :key="s.line" class="is-size-5">{{ s.line }}</p>
-        <div v-for="s in slideContent" :key="s.img">
-            <img :src="s.img">
+        <div v-for="(s, index) in slideContent" :key="index" style="margin: 5px 0;">
+            <p class="is-size-5" v-if="s.line">{{ s.line }}</p>
+            <img :src="s.img" v-if="s.img" style="margin: 5px 0;">
+            <ul v-if="s.list" class="list" style="margin: 5px 0;">
+                <li v-for="(sl, i) in s.list" :key="i" class="list-item">{{ sl.item }}</li>
+            </ul>
         </div>
     </section>
 </template>

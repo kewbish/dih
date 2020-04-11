8bdfa125a52167714ed5a50603d04b27dd4b6d3c
Kewbish <kewbish@gmail.com>
2020-04-10 20:01:15 -0700

    Begin Timer work

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,21 +1,47 @@
 <template>
-    <div class="message is-info">
-        <div class="message-header">
-            <p>{{ title }}</p>
-        </div>
-        <div class="columns">
-            <div class="column">
-                <p>{{ subtitle }}</p>
-            </div>
-            <div class="column">
-                <!--Implement timer-->
-            </div>
-        </div>
+  <div class="message is-info">
+    <div class="message-header">
+      <p>{{ title }}</p>
     </div>
+    <div class="columns">
+      <div class="column">
+          <div class="box">
+              <p class="has-text-white">{{ subtitle }}</p>
+          </div>
+      </div>
+      <div class="column">
+        <div class="box">
+            <h1 class="title has-text-white"></h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['duration', 'title', 'subtitle']
-}
+  props: ["durationInSecs", "title", "subtitle"],
+  computed: {
+    formatTime() {
+      var timeLeft = this.durationInSecs;
+      var minutes = Math.floor(timeLeft / 60);
+      var seconds = timeLeft % 60;
+      if (seconds < 10){
+          seconds = `0${seconds}`;
+      }
+      return `${minutes}:${seconds}`
+    },
+    timeLeft(){
+        return this.timeLeft //
+    }
+  },
+  data(){
+      return timerInt = null;
+  },
+  methods:{
+      startTimer(){
+          this.timerInt = setInterval(() => (this.))
+      }
+  }
+};
 </script>

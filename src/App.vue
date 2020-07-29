0dad422ae3ace5e709c69e1468cf0875d8cc9455
Kewbish <kewbish@gmail.com>
2020-07-28 19:31:21 -0700

    Fix NaN issues

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <input v-if="!isNaN(count)" type="text" class="input fixed-input" v-model="count" />
-    <button v-if="count > 0" @click="countDown" class="button is-info fixed-back is-small">&lt;</button>
-    <button v-if="count < 22" @click="countUp" class="button is-info fixed-right is-small">&gt;</button>
+    <input
+      v-if="$router.currentRoute.path !== '/' && $router.currentRoute.path !== '/login'"
+      type="text"
+      class="input fixed-input"
+      v-model.lazy="count"
+    />
+    <button
+      v-if="parseInt(count) > 0"
+      @click="countDown"
+      class="button is-info fixed-back is-small"
+    >&lt;</button>
+    <button
+      v-if="parseInt(count) < 22"
+      @click="countUp"
+      class="button is-info fixed-right is-small"
+    >&gt;</button>
     <div class="fixed-right-bottom">
       <a
         href="https://canadalearningcode.secure.nonprofitsoapbox.com/clc-donate"
@@ -17,14 +30,14 @@
 export default {
   methods: {
     countUp() {
-      if (this.count < 22) {
-        var newU = this.count + 1;
+      if (parseInt(this.count) < 22) {
+        var newU = parseInt(this.count) + 1;
         this.$router.push(`/${newU}`);
       }
     },
     countDown() {
-      if (this.count > 0) {
-        var newU = this.count - 1;
+      if (parseInt(this.count) > 0) {
+        var newU = parseInt(this.count) - 1;
         this.$router.push(`/${newU}`);
       }
     },
@@ -32,7 +45,7 @@ export default {
   computed: {
     count: {
       get: function () {
-        return parseInt(this.$route.fullPath.replace("/", ""));
+        return this.$route.fullPath.replace("/", "");
       },
       set: function (val) {
         this.$router.push(`/${val}`);

5a6b079a4f267e59858bfaf32bbeffc47ba50a5a
Kewbish <kewbish@gmail.com>
2020-09-11 19:11:32 -0700

    Remove CLC references

--- src/components/slides/z22.vue
+++ src/components/slides/z22.vue
@@ -11,9 +11,8 @@ export default {
       \n- \`<iframe>\` and rich content
       \n- Flexbox
       \nIf you'd like to investigate these technologies further, check out the following resources: [Khan Academy](https://www.khanacademy.org/computing/computer-programming/html-css), [GitHub Labs](https://lab.github.com/githubtraining/introduction-to-html), and [Traversy Media](https://www.youtube.com/watch?v=UB1O30fR-EE&list=PLillGF-RfqbZTASqIqdvm1R5mLrQq79CU).
-      \nThat just about does it for this lesson. Again, if you'd like to support Canada Learning Code by making a donation, click the red button in the bottom right.
-      \nAs well, I've created a GitHub repo at the link below. That's the place to ask any questions and post links to your finished sites in the issues. [The repo.](https://github.com/kewbish/diveintohtml)
-      \nIf you have any questions, feel free to get in touch via the form on my website. Alternatively, find me on the Interwebs: [my site](https://kewbish.github.io), [my GitHub](https://github.com/kewbish), and [my Dev.to](https://dev.to/kewbish).
+      \nIf you're interested, I've created a GitHub repo at the link below. That's the place to ask any questions and post links to your finished sites in the issues. [Check it out here!](https://github.com/kewbish/diveintohtml) The source of this site is also available on GitHub [here](https://github.com/kewbish/sjap).
+      \nThis site was made by Kewbish - find me on the Interwebs: [my site](https://kewbi.sh), [my GitHub](https://github.com/kewbish), and [my Dev.to](https://dev.to/kewbish).
       \nThanks for learning! If you found this useful, please share this site with your friends, and help guide them in building a web presence as well.
       \n- [Share on Reddit](http://www.reddit.com/submit?url=https://diveintoht.ml&title=Dive%20Into%20HTML%20-%20A%20great%20site%20to%20learn%20HTML!)
       \n- [Share on Twitter](https://twitter.com/intent/tweet?text=diveintoht.ml%20-%20a%20great%20site%20to%20learn%20HTML!)

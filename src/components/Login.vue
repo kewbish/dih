6d6abe3e0d82916320f81ed155dca917aafa6661
Kewbish <kewbish@gmail.com>
2020-09-26 19:49:15 -0700

    Add email

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,11 +12,28 @@
             <em>may</em> work on mobile, but it's difficult to view the code snippets.
           </p>
           <div class="tile is-ancestor">
-              <div class="tile is-4 is-vertical is-parent">
+              <div class="tile is-4 is-horizontal is-parent">
                 <div class="tile is-child box">
                   <a class="button" style="margin-bottom:10px" @click="loginGoogle">Log In - Gmail</a><br>
                   <a class="button" style="margin-bottom:10px" @click="loginGitHub">Log In - GitHub</a><br>
                   <a class="button" style="margin-bottom:10px" @click="loginAnon">Continue without logging in</a>
+                </div>
+                <div class="tile is-child box">
+                  <div class="field">
+                      <label class="label has-text-white">Email</label>
+                      <div class="control"><input type="email" v-model="email" placeholder="Your email."></div>
+                  </div>
+                  <div class="field">
+                      <label class="label has-text-white">Password</label>
+                      <div class="control"><input type="text" v-model="pass" placeholder="Password."></div>
+                  </div>
+                  <div class="field">
+                      <label class="label has-text-white">Confirm password</label>
+                      <div class="control"><input type="text" v-model="passrep" placeholder="Confirm password."></div>
+                  </div>
+                  <div class="field">
+                      <div class="control"><button class="button is-dark" @click="loginEmail()">Log In / Create</button></div>
+                  </div>
                 </div>
              </div>
           </div>
@@ -30,6 +47,13 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 export default {
+  data: function () {
+    return {
+        email: null,
+        pass: null,
+        passrep: null
+    }
+  },
   methods: {
     loginGoogle() {
       const provider = new firebase.auth.GoogleAuthProvider();
@@ -46,6 +70,25 @@ export default {
                 firebase
                 .auth()
                 .signInAnonymously()
+                .then(() => {
+                  this.$router.push(lastRoute != null ? lastRoute : "/0");
+                })
+                .catch((err) => {
+                  console.log("There was an error.", err);
+                });
+            } else {
+               this.$router.push(lastRoute != null ? lastRoute : "/0");                                 
+            }
+        });
+    },
+    loginEmail() {
+        const lastRoute = localStorage.getItem("page");
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                if (this.pass != this.passrep) {
+                    console.log("There was an error.");
+                }
+                firebase.auth().signInWithEmailAndPassword(this.email, this.pass)
                 .then(() => {
                   this.$router.push(lastRoute != null ? lastRoute : "/0");
                 })

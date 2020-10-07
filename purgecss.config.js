afaff8b276332f11457b65a2698c912c484730cf
Kewbish <kewbish@gmail.com>
2020-10-07 08:01:03 -0700

    Add PurgeCSS config and generate

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ['src/**/*.vue', 'public/index.html'],
+  css: ['public/backupcss/bulma.min.css', 'public/backupcss/bulma-prefers-dark.css'],
+  output: 'public',
+  safelist: ['is-active']
+}

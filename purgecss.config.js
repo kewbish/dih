dff381ea93f5ef89d60fb6df48782a3d3eb7effd
Kewbish <kewbish@gmail.com>
2020-10-08 19:52:27 -0700

    Add codeblock CSS fix

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,5 @@ module.exports = {
   content: ['src/**/*.vue', 'public/index.html'],
   css: ['public/backupcss/bulma.min.css', 'public/backupcss/bulma-prefers-dark.css'],
   output: 'public',
-  safelist: ['is-active']
+  safelist: ['is-active', 'pre', 'code', 'content']
 }

eb83be71f0d4a44fff539a3e1f67ed28f8394795
Kewbish <kewbish@gmail.com>
2020-10-16 20:05:47 -0700

    Basic editor in place

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="section">
         <h1 class="title">diveintoht.ml editor</h1>
-        <div class="box">
-            <div id="editor-html">
+        <div class="notification is-info" style="height: 63vh;">
+            <div id="editor-html" style="height: 55vh;">
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import * as monaco from 'monaco-editor'
-
-monaco.editor.create(document.getElementById('editor-html'), {
-  value: '<!--Get started!🌊-->',
-  language: 'html'
-});
-
+import * as monaco from 'monaco-editor';
 export default {
-    
+   mounted() {
+    monaco.editor.create(document.getElementById('editor-html'), {
+      value: '<!--Get started!🌊-->',
+      language: 'html',
+      theme: 'vs-dark'
+    });
+   }
 }
 </script>

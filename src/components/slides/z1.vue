5755001d95dfe50796bf5fb8db42510463b6902e
Kewbish <kewbish@gmail.com>
2020-10-25 19:58:48 -0700

    Change references to VSCode to editor

--- src/components/slides/z1.vue
+++ src/components/slides/z1.vue
@@ -7,13 +7,8 @@ export default {
   data() {
     return {
       slide: `Every website starts with an index.html file. This is what web browsers look for to start rendering your website from a site's files when it loads up.
-      \nCreate a folder on your computer, and open this folder with VSCode by clicking this button:
-      \n![Figure 1: Open the folder you just created.](https://i.imgur.com/QseX16F.png)
-      \n*Figure 1: Open the folder you just created.*
-      \nNow, it's time to create an index.html file.
-      \n![Figure 2: Create a new file, and call it index.html.](https://code.visualstudio.com/assets/docs/nodejs/nodejs/toolbar-new-file.png)
-      \n*Figure 2: Create a new file, and call it index.html.*
-      \nYou'll need to do the same with a main.css file. Create a main.css file, just like you created your index.html file.`,
+      \nIf you're using the diveintoht.ml editor, this is automatically created for you in the HTML tab. The same goes for a main.css, which is available to edit under the CSS tab.
+      \nIf you're using another editor, create a folder, and inside it, index.html and main.css files.`,
     };
   },
 };

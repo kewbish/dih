72df8d63ec96582ab0c36d4d2dd1132b69878e42
Kewbish <kewbish@gmail.com>
2020-10-31 17:07:55 -0700

    Remove Firebase

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -26,67 +26,65 @@ import * as monaco from 'monaco-editor';
 export default {
     data() {
         return {
-editors: null,
-             editorHtml: null,
-             editorCss: null,
-             preview: {"content": null, "isAct": false}
+            editors: null,
+            editorHtml: null,
+            editorCss: null,
+            preview: {"content": null, "isAct": false}
         }
     },
-        mounted() {
-            this.editors = monaco.editor.create(document.getElementById('editors'), {
-theme: 'vs-dark'
-});
-const html = localStorage.getItem('html') || '<!DOCTYPE html>\n<html lang="en">\n<head>\t\n<meta charset="UTF-8">\t\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\t\n<title></title>\n</head>\n<body>\n</body>\n</html>';
-const css = localStorage.getItem('css') || "/* Get started 🌊 */";
-this.editorHtml = monaco.editor.createModel(html, "html");
-this.editorCss = monaco.editor.createModel(css, "css");
-this.editors.setModel(this.editorHtml);
-this.editors.onDidChangeModelContent(() => {
-        this.setMode('preview');
+    mounted() {
+        this.editors = monaco.editor.create(document.getElementById('editors'), {theme: 'vs-dark'});
+        const html = localStorage.getItem('html') || '<!DOCTYPE html>\n<html lang="en">\n<head>\t\n<meta charset="UTF-8">\t\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\t\n<title></title>\n</head>\n<body>\n</body>\n</html>';
+        const css = localStorage.getItem('css') || "/* Get started 🌊 */";
+        this.editorHtml = monaco.editor.createModel(html, "html");
+        this.editorCss = monaco.editor.createModel(css, "css");
+        this.editors.setModel(this.editorHtml);
+        this.editors.onDidChangeModelContent(() => {
+            this.setMode('preview');
         });
-this.editorHtml.onDidChangeContent(() => {
-        this.localSave('html');
+        this.editorHtml.onDidChangeContent(() => {
+            this.localSave('html');
         });
-this.editorCss.onDidChangeContent(() => {
-        this.localSave('css');
+        this.editorCss.onDidChangeContent(() => {
+            this.localSave('css');
         });
-},
-methods: {
-             setMode(model) {
-                 if (model == 'html') {
-                     this.editors.setModel(this.editorHtml);
-                 }
-                 else if (model == 'css') {
-                     this.editors.setModel(this.editorCss);
-                 }
-                 else if (model == 'preview') {
-                     const html = this.editorHtml.getValue();
-                     const css = this.editorCss.getValue();
-                     this.preview['content'] = html + `<style>${css}</style>`;
-                 }
-             },
-             localSave(mode) {
-                 if (mode == 'html') {
-                     const html = this.editorHtml.getValue();
-                     localStorage.setItem('html', html);
-                 }
-                 else if (mode == 'css') {
-                     const css = this.editorCss.getValue();
-                     localStorage.setItem('css', css);
-                 }
-             },
-             togglePrev() {
-                 this.preview['isAct'] = !this.preview['isAct'];
-             },
-             exportFiles () {
-                 const html = document.getElementById("preview").srcdoc;
-                 console.log(html);
-                 var link = document.createElement('a');
-                 link.setAttribute('download', 'index.html');
-                 link.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(html));
-                 link.click();
-             }
-         }
+    },
+    methods: {
+        setMode(model) {
+            if (model == 'html') {
+                this.editors.setModel(this.editorHtml);
+            }
+            else if (model == 'css') {
+                this.editors.setModel(this.editorCss);
+            }
+            else if (model == 'preview') {
+                const html = this.editorHtml.getValue();
+                const css = this.editorCss.getValue();
+                this.preview['content'] = html + `<style>${css}</style>`;
+            }
+        },
+        localSave(mode) {
+            if (mode == 'html') {
+                const html = this.editorHtml.getValue();
+                localStorage.setItem('html', html);
+            }
+            else if (mode == 'css') {
+                const css = this.editorCss.getValue();
+                localStorage.setItem('css', css);
+            }
+        },
+        togglePrev() {
+            this.preview['isAct'] = !this.preview['isAct'];
+        },
+        exportFiles () {
+            const html = document.getElementById("preview").srcdoc;
+            console.log(html);
+            var link = document.createElement('a');
+            link.setAttribute('download', 'index.html');
+            link.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(html));
+            link.click();
+        }
+    }
 }
 </script>
 

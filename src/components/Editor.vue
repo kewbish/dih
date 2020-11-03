72fde0bf2b3273785d21b60f6c34cd6795eddb6a
Kewbish <kewbish@gmail.com>
2020-11-02 19:42:38 -0800

    Run autoformatter

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,108 +1,121 @@
 <template>
-    <div class="section hero is-dark">
-        <h1 class="title">The Editor.</h1>
-        <div class="hero-body px-0 py-3">
-            <div class="box" style="height: 100%;">
-                <div class="tabs">
-                    <ul>
-                        <li @click="setMode('html')" class="is-active"><a>HTML</a></li>
-                        <li @click="setMode('css')"><a>CSS</a></li>
-                        <li @click="setMode('preview');togglePrev()"><a>Toggle preview</a></li>
-                        <li @click="exportFiles()"><a>Export Files</a></li>
-                    </ul>
-                </div>
-                <div id="editors" style="height: 55vh;"></div>
-                <div v-if="preview['isAct']">
-                    <hr>
-                    <h1 class="title is-4">Preview</h1>
-                    <iframe class="box mt-2" style="width: 100;" id="preview" :srcdoc="preview['content']"></iframe>
-                </div>
-            </div>
-            <button class="button" @click="back()">📚 Go back</button>
+  <div class="section hero is-dark">
+    <h1 class="title">The Editor.</h1>
+    <div class="hero-body px-0 py-3">
+      <div class="box" style="height: 100%;">
+        <div class="tabs">
+          <ul>
+            <li @click="setMode('html')" class="is-active">
+              <a>HTML</a>
+            </li>
+            <li @click="setMode('css')">
+              <a>CSS</a>
+            </li>
+            <li @click="setMode('preview');togglePrev()">
+              <a>Toggle preview</a>
+            </li>
+            <li @click="exportFiles()">
+              <a>Export Files</a>
+            </li>
+          </ul>
         </div>
+        <div id="editors" style="height: 55vh;"></div>
+        <div v-if="preview['isAct']">
+          <hr />
+          <h1 class="title is-4">Preview</h1>
+          <iframe class="box mt-2" style="width: 100;" id="preview" :srcdoc="preview['content']"></iframe>
+        </div>
+      </div>
+      <button class="button" @click="back()">📚 Go back</button>
     </div>
+  </div>
 </template>
 <script>
-import * as monaco from 'monaco-editor';
+import * as monaco from "monaco-editor";
 export default {
-    data() {
-        return {
-            editors: null,
-            editorHtml: null,
-            editorCss: null,
-            preview: {"content": null, "isAct": false}
-        }
-    },
-    mounted() {
-        this.editors = monaco.editor.create(document.getElementById('editors'), {theme: 'vs-dark'});
-        const html = localStorage.getItem('html') || '<!DOCTYPE html>\n<html lang="en">\n<head>\t\n<meta charset="UTF-8">\t\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\t\n<title></title>\n</head>\n<body>\n</body>\n</html>';
-        const css = localStorage.getItem('css') || "/* Get started 🌊 */";
-        this.editorHtml = monaco.editor.createModel(html, "html");
-        this.editorCss = monaco.editor.createModel(css, "css");
+  data() {
+    return {
+      editors: null,
+      editorHtml: null,
+      editorCss: null,
+      preview: { content: null, isAct: false },
+    };
+  },
+  mounted() {
+    this.editors = monaco.editor.create(document.getElementById("editors"), {
+      theme: "vs-dark",
+    });
+    const html =
+      localStorage.getItem("html") ||
+      '<!DOCTYPE html>\n<html lang="en">\n<head>\t\n<meta charset="UTF-8">\t\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\t\n<title></title>\n</head>\n<body>\n</body>\n</html>';
+    const css = localStorage.getItem("css") || "/* Get started 🌊 */";
+    this.editorHtml = monaco.editor.createModel(html, "html");
+    this.editorCss = monaco.editor.createModel(css, "css");
+    this.editors.setModel(this.editorHtml);
+    this.editors.onDidChangeModelContent(() => {
+      this.setMode("preview");
+    });
+    this.editorHtml.onDidChangeContent(() => {
+      this.localSave("html");
+    });
+    this.editorCss.onDidChangeContent(() => {
+      this.localSave("css");
+    });
+    this.keyShorts = function (e) {
+      if (e.key === "V" && e.shiftKey && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        this.togglePrev();
+      }
+    };
+    document.addEventListener("keydown", this.keyShorts.bind(this));
+  },
+  methods: {
+    setMode(model) {
+      if (model == "html") {
         this.editors.setModel(this.editorHtml);
-        this.editors.onDidChangeModelContent(() => {
-            this.setMode('preview');
-        });
-        this.editorHtml.onDidChangeContent(() => {
-            this.localSave('html');
-        });
-        this.editorCss.onDidChangeContent(() => {
-            this.localSave('css');
-        });
-        this.keyShorts = function(e) {
-            if (e.key === "V" && e.shiftKey && (e.ctrlKey || e.metaKey)) {
-                e.preventDefault();
-                this.togglePrev();
-            }
-        };
-        document.addEventListener('keydown', this.keyShorts.bind(this));
+      } else if (model == "css") {
+        this.editors.setModel(this.editorCss);
+      } else if (model == "preview") {
+        const html = this.editorHtml.getValue();
+        const css = this.editorCss.getValue();
+        this.preview["content"] = html + `<style>${css}</style>`;
+      }
     },
-    methods: {
-        setMode(model) {
-            if (model == 'html') {
-                this.editors.setModel(this.editorHtml);
-            }
-            else if (model == 'css') {
-                this.editors.setModel(this.editorCss);
-            }
-            else if (model == 'preview') {
-                const html = this.editorHtml.getValue();
-                const css = this.editorCss.getValue();
-                this.preview['content'] = html + `<style>${css}</style>`;
-            }
-        },
-        localSave(mode) {
-            if (mode == 'html') {
-                const html = this.editorHtml.getValue();
-                localStorage.setItem('html', html);
-            }
-            else if (mode == 'css') {
-                const css = this.editorCss.getValue();
-                localStorage.setItem('css', css);
-            }
-        },
-        togglePrev() {
-            this.preview['isAct'] = !this.preview['isAct'];
-        },
-        exportFiles () {
-            const html = document.getElementById("preview").srcdoc;
-            console.log(html);
-            var link = document.createElement('a');
-            link.setAttribute('download', 'index.html');
-            link.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(html));
-            link.click();
-        },
-        back () {
-            const lastRoute = localStorage.getItem("page");
-            this.$router.push(lastRoute != "undefined" || lastRoute != null ? lastRoute : "/0")
-            .catch((err) => {
-              console.log("There was an error.", err);
-            });
-        }
+    localSave(mode) {
+      if (mode == "html") {
+        const html = this.editorHtml.getValue();
+        localStorage.setItem("html", html);
+      } else if (mode == "css") {
+        const css = this.editorCss.getValue();
+        localStorage.setItem("css", css);
+      }
     },
-    beforeDestroy() {
-        document.removeEventListener('keydown', this.keyShorts);
-    }
-}
+    togglePrev() {
+      this.preview["isAct"] = !this.preview["isAct"];
+    },
+    exportFiles() {
+      const html = document.getElementById("preview").srcdoc;
+      console.log(html);
+      var link = document.createElement("a");
+      link.setAttribute("download", "index.html");
+      link.setAttribute(
+        "href",
+        "data:text/html;charset=utf-8," + encodeURIComponent(html)
+      );
+      link.click();
+    },
+    back() {
+      const lastRoute = localStorage.getItem("page");
+      this.$router
+        .push(lastRoute != "undefined" || lastRoute != null ? lastRoute : "/0")
+        .catch((err) => {
+          console.log("There was an error.", err);
+        });
+    },
+  },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.keyShorts);
+  },
+};
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,77 +8,113 @@
         <div class="message-body">
           <p class="subtitle">We do not collect any information other than your display name.</p>
           <div class="tile is-ancestor">
-              <div class="tile is-parent">
-                <div class="tile is-child box">
-                  <div class="login" v-if="loginSwitch">
-                      <h1 class="title is-4">Login <a class="switch" @click="switchLogin">or create</a></h1>
-                      <div class="field">
-                          <label class="label">Email</label>
-                          <div class="control"><input class="input" type="email" v-model="email" placeholder="Your email."></div>
-                      </div>
-                      <div class="field">
-                          <label class="label">Password</label>
-                          <div class="control"><input class="input" type="password" v-model="pass" placeholder="Password."></div>
-                      </div>
-                      <div class="field">
-                          <div class="control"><button class="button" style="margin-right: 1%;" @click="loginEmail()">Log In</button> 
-                          <button class="button" @click="forgotEmailSwitch()">Forgot password?</button>
-                          </div>
-                      </div>
+            <div class="tile is-parent">
+              <div class="tile is-child box">
+                <div class="login" v-if="loginSwitch">
+                  <h1 class="title is-4">
+                    Login
+                    <a class="switch" @click="switchLogin">or create</a>
+                  </h1>
+                  <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                      <input class="input" type="email" v-model="email" placeholder="Your email." />
                     </div>
-                    <div v-else-if="forgot">
-                      <h1 class="title is-4">Reset password <a class="switch" @click="switchLogin">or login</a></h1>
-                      <div class="field">
-                          <label class="label">Email</label>
-                          <div class="control"><input class="input" type="email" v-model="email" placeholder="Your email."></div>
-                      </div>
-                      <div class="field">
-                          <div class="control"><button class="button" style="margin-right: 1%;" @click="forgotEmail()">Send reset email</button> 
-                          </div>
-                      </div>
+                  </div>
+                  <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                      <input class="input" type="password" v-model="pass" placeholder="Password." />
                     </div>
-                    <div class="create" v-else>
-                      <h1 class="title is-4">Create account <a class="switch" @click="switchLogin">or login</a></h1>
-                      <div class="field">
-                          <label class="label">Email</label>
-                          <div class="control"><input class="input" type="email" v-model="email" placeholder="Your email."></div>
-                      </div>
-                      <div class="field">
-                          <label class="label">Password</label>
-                          <div class="control"><input class="input" type="password" v-model="pass" placeholder="Password."></div>
-                      </div>
-                      <div class="field">
-                          <label class="label">Confirm password</label>
-                          <div class="control"><input class="input" type="password" v-model="passrep" placeholder="Confirm password."></div>
-                      </div>
-                      <div class="field">
-                          <div class="control"><button class="button" @click="createEmail()">Create</button></div>
-                      </div>
-                     </div>
                   </div>
-               </div>
-              <div class="tile is-4 is-parent">
-                  <div class="tile is-child">
-                      <div class="box">
-                          <h1 class="title is-4">Other options</h1>
-                          <a class="button" style="margin-bottom:10px" @click="loginGoogle">Log In - Gmail</a><br>
-                          <a class="button" style="margin-bottom:10px" @click="loginGitHub">Log In - GitHub</a><br> 
-                          <a class="button" style="margin-bottom:10px" @click="loginAnon">Anonymous - no info</a>
-                      </div>
+                  <div class="field">
+                    <div class="control">
+                      <button class="button" style="margin-right: 1%;" @click="loginEmail()">Log In</button>
+                      <button class="button" @click="forgotEmailSwitch()">Forgot password?</button>
+                    </div>
                   </div>
+                </div>
+                <div v-else-if="forgot">
+                  <h1 class="title is-4">
+                    Reset password
+                    <a class="switch" @click="switchLogin">or login</a>
+                  </h1>
+                  <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                      <input class="input" type="email" v-model="email" placeholder="Your email." />
+                    </div>
+                  </div>
+                  <div class="field">
+                    <div class="control">
+                      <button
+                        class="button"
+                        style="margin-right: 1%;"
+                        @click="forgotEmail()"
+                      >Send reset email</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="create" v-else>
+                  <h1 class="title is-4">
+                    Create account
+                    <a class="switch" @click="switchLogin">or login</a>
+                  </h1>
+                  <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                      <input class="input" type="email" v-model="email" placeholder="Your email." />
+                    </div>
+                  </div>
+                  <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                      <input class="input" type="password" v-model="pass" placeholder="Password." />
+                    </div>
+                  </div>
+                  <div class="field">
+                    <label class="label">Confirm password</label>
+                    <div class="control">
+                      <input
+                        class="input"
+                        type="password"
+                        v-model="passrep"
+                        placeholder="Confirm password."
+                      />
+                    </div>
+                  </div>
+                  <div class="field">
+                    <div class="control">
+                      <button class="button" @click="createEmail()">Create</button>
+                    </div>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div class="tile is-4 is-parent">
+              <div class="tile is-child">
+                <div class="box">
+                  <h1 class="title is-4">Other options</h1>
+                  <a class="button" style="margin-bottom:10px" @click="loginGoogle">Log In - Gmail</a>
+                  <br />
+                  <a class="button" style="margin-bottom:10px" @click="loginGitHub">Log In - GitHub</a>
+                  <br />
+                  <a
+                    class="button"
+                    style="margin-bottom:10px"
+                    @click="loginAnon"
+                  >Anonymous - no info</a>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="message is-danger" v-if="err">
-             <div class="message-header">Error</div>
-             <div class="message-body">
-               {{err}}
-             </div>
+            <div class="message-header">Error</div>
+            <div class="message-body">{{err}}</div>
           </div>
           <div class="message is-success" v-if="message">
-             <div class="message-header">Success!</div>
-             <div class="message-body">
-               {{message}}
-             </div>
+            <div class="message-header">Success!</div>
+            <div class="message-body">{{message}}</div>
           </div>
         </div>
       </div>
@@ -92,14 +128,14 @@ import "firebase/auth";
 export default {
   data: function () {
     return {
-        loginSwitch: true,
-        forgot: false,
-        email: null,
-        pass: null,
-        passrep: null,
-        err: null,
-        message: null
-    }
+      loginSwitch: true,
+      forgot: false,
+      email: null,
+      pass: null,
+      passrep: null,
+      err: null,
+      message: null,
+    };
   },
   methods: {
     switchLogin() {
@@ -111,65 +147,69 @@ export default {
       this.login(provider);
     },
     loginGitHub() {
-        const provider = new firebase.auth.GithubAuthProvider();
-        this.login(provider);
+      const provider = new firebase.auth.GithubAuthProvider();
+      this.login(provider);
     },
     loginAnon() {
-        const lastRoute = localStorage.getItem("page");
-        const user = firebase.auth().user;
-        if (!user) {
-            firebase
-            .auth()
-            .signInAnonymously()
-            .then(() => {
-              this.$router.push(lastRoute != null ? lastRoute : "/0")
+      const lastRoute = localStorage.getItem("page");
+      const user = firebase.auth().user;
+      if (!user) {
+        firebase
+          .auth()
+          .signInAnonymously()
+          .then(() => {
+            this.$router
+              .push(lastRoute != null ? lastRoute : "/0")
               .catch((err) => {
                 console.log("There was an error.", err);
               });
-            })
-            .catch((err) => {
-              console.log("There was an error.", err);
-              this.err = `${err} - please try again!`;
-            });
-        } else {
-           this.$router.push(lastRoute != null ? lastRoute : "/0")
-           .catch((err) => {
-             console.log("There was an error.", err);
-           });
-        }
+          })
+          .catch((err) => {
+            console.log("There was an error.", err);
+            this.err = `${err} - please try again!`;
+          });
+      } else {
+        this.$router.push(lastRoute != null ? lastRoute : "/0").catch((err) => {
+          console.log("There was an error.", err);
+        });
+      }
     },
     loginEmail() {
-        const lastRoute = localStorage.getItem("page");
-        const user = firebase.auth().user;
-        if (!user) {
-            firebase.auth().signInWithEmailAndPassword(this.email, this.pass)
-            .then(() => {
-              this.$router.push(lastRoute != null ? lastRoute : "/0")
+      const lastRoute = localStorage.getItem("page");
+      const user = firebase.auth().user;
+      if (!user) {
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email, this.pass)
+          .then(() => {
+            this.$router
+              .push(lastRoute != null ? lastRoute : "/0")
               .catch((err) => {
                 console.log("There was an error.", err);
               });
-            })
-            .catch((err) => {
-              console.log("There was an error.", err);
-              this.err = `${err} - please try again!`;
-            });
-        } else {
-           this.$router.push(lastRoute != null ? lastRoute : "/0")
-           .catch((err) => {
-             console.log("There was an error.", err);
-           });
-        }
+          })
+          .catch((err) => {
+            console.log("There was an error.", err);
+            this.err = `${err} - please try again!`;
+          });
+      } else {
+        this.$router.push(lastRoute != null ? lastRoute : "/0").catch((err) => {
+          console.log("There was an error.", err);
+        });
+      }
     },
     forgotEmailSwitch() {
-        this.loginSwitch = false;
-        this.forgot = true;
+      this.loginSwitch = false;
+      this.forgot = true;
     },
     forgotEmail() {
-        firebase.auth().sendPasswordResetEmail(this.email)
+      firebase
+        .auth()
+        .sendPasswordResetEmail(this.email)
         .then(() => {
-            this.message = "A password reset was sent.";
-            this.loginSwitch = true;
-            this.forgot = false;
+          this.message = "A password reset was sent.";
+          this.loginSwitch = true;
+          this.forgot = false;
         })
         .catch((err) => {
           console.log("There was an error.", err);
@@ -177,79 +217,85 @@ export default {
         });
     },
     createEmail() {
-        const lastRoute = localStorage.getItem("page");
-        const user = firebase.auth().user;
-        if (!user) {
-            if (this.pass != this.passrep) {
-                console.log("There was an error.");
-                this.err = "Passwords do not match - please try again!";
-                return;
-            }
-            firebase.auth().createUserWithEmailAndPassword(this.email, this.pass)
-            .then(() => {
-              this.$router.push(lastRoute != null ? lastRoute : "/0")
+      const lastRoute = localStorage.getItem("page");
+      const user = firebase.auth().user;
+      if (!user) {
+        if (this.pass != this.passrep) {
+          console.log("There was an error.");
+          this.err = "Passwords do not match - please try again!";
+          return;
+        }
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.pass)
+          .then(() => {
+            this.$router
+              .push(lastRoute != null ? lastRoute : "/0")
               .catch((err) => {
                 console.log("There was an error.", err);
               });
-            })
-            .catch((err) => {
-              if (err == "auth/email-already-in-use") {
-                firebase.auth().signInWithEmailAndPassword(this.email, this.pass)
+          })
+          .catch((err) => {
+            if (err == "auth/email-already-in-use") {
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.pass)
                 .then(() => {
-                  this.$router.push(lastRoute != null ? lastRoute : "/0")
-                  .catch((err) => {
-                    console.log("There was an error.", err);
-                  });
-                })                                                          
-                .catch((err) => {                                           
+                  this.$router
+                    .push(lastRoute != null ? lastRoute : "/0")
+                    .catch((err) => {
+                      console.log("There was an error.", err);
+                    });
+                })
+                .catch((err) => {
                   console.log("There was an error.", err);
                   this.err = `${err} - please try again!`;
                 });
-              } else {
-                  console.log("There was an error.", err);
-                  this.err = `${err} - please try again!`;
-              }
-            });
-        } else {
-           this.$router.push(lastRoute != null ? lastRoute : "/0") 
-           .catch((err) => {
-             console.log("There was an error.", err);
-           });
-        }
+            } else {
+              console.log("There was an error.", err);
+              this.err = `${err} - please try again!`;
+            }
+          });
+      } else {
+        this.$router.push(lastRoute != null ? lastRoute : "/0").catch((err) => {
+          console.log("There was an error.", err);
+        });
+      }
     },
     login(provider) {
-        const lastRoute = localStorage.getItem("page");
-        const user = firebase.auth().user;
-        if (!user) {
-            firebase
-            .auth()
-            .signInWithPopup(provider)
-            .then(() => {
-              this.$router.push(lastRoute != null ? lastRoute : "/0")
+      const lastRoute = localStorage.getItem("page");
+      const user = firebase.auth().user;
+      if (!user) {
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then(() => {
+            this.$router
+              .push(lastRoute != null ? lastRoute : "/0")
               .catch((err) => {
                 console.log("There was an error.", err);
               });
-            })
-            .catch((err) => {
-              console.log("There was an error.", err);
-              this.err = `${err} - please try again!`;
-            });
-        } else {
-           this.$router.push(lastRoute != null ? lastRoute : "/0")
-           .catch((err) => {
-             console.log("There was an error.", err);
-           });
-        }
-    }
+          })
+          .catch((err) => {
+            console.log("There was an error.", err);
+            this.err = `${err} - please try again!`;
+          });
+      } else {
+        this.$router.push(lastRoute != null ? lastRoute : "/0").catch((err) => {
+          console.log("There was an error.", err);
+        });
+      }
+    },
   },
   beforeCreate: function () {
     const lastRoute = localStorage.getItem("page");
     const user = firebase.auth().currentUser;
     if (user && lastRoute) {
-      this.$router.push(lastRoute != "undefined" || lastRoute != null ? lastRoute : "/0")
-      .catch((err) => {
-        console.log("There was an error.", err);
-      });
+      this.$router
+        .push(lastRoute != "undefined" || lastRoute != null ? lastRoute : "/0")
+        .catch((err) => {
+          console.log("There was an error.", err);
+        });
     }
   },
 };
@@ -257,7 +303,7 @@ export default {
 
 <style scoped>
 .switch:hover {
-    color: #209cee !important;
-    transition: 0.3s ease all;
+  color: #209cee !important;
+  transition: 0.3s ease all;
 }
 </style>

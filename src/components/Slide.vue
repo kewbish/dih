cec9b280aed518d9c9243620c5546b4fc67c360f
Kewbish <kewbish@gmail.com>
2020-12-05 08:59:30 -0800

    Add slot to slide component, fix 14 playground

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -5,6 +5,7 @@
         <h1 class="title is-1">{{ slideSubject }}</h1>
         <div class="content is-medium">
           <VueShowdown :markdown="slideContent" :vueTemplate="true" />
+          <slot></slot>
         </div>
       </section>
     </div>
